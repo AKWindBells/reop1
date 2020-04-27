09deca8d80bbfc541be905076698f5fba0cc5d6c
Joe <386040702@qq.com>
2020-04-27 17:38:03 +0800

    1111111111111

---
+++ cloud-provider-payment8001/src/main/java/com/joe/springcloud/PaymengMain8001.java
@@ -0,0 +1,12 @@
+package com.joe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymengMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymengMain8001.class,args);
+
+    }
+}
